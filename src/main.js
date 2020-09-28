7c8b79ac97448c1ed68ac2eda28277a27ca84fed
周健煌 <1127893332@qq.com>
2020-09-28 22:35:11 +0800

    rem的屏幕适配以及注释

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant'
-import 'vant/lib/index.css'
+import Vant from 'vant' // 引入vant插件
+import 'vant/lib/index.css' // 引入vant样式
+import 'amfe-flexible' // 引入根据当前屏幕的变化而变化的font-size插件
 
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 
