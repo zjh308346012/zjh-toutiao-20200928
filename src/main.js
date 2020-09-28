3389c3c60780d3fc5ede0e618438be7660e61979
周健煌 <1127893332@qq.com>
2020-09-28 22:25:29 +0800

    引入Vant组件以及样式,并全局注册

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 
 Vue.config.productionTip = false
 
