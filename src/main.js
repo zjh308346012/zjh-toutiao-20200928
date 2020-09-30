a186d098e2c5e5c181138a2abca1f25279e0c138
周健煌 <1127893332@qq.com>
2020-09-30 21:51:32 +0800

    引入全局的style样式

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import Vant from 'vant' // 引入vant插件
 import 'vant/lib/index.css' // 引入vant样式
 import 'amfe-flexible' // 引入根据当前屏幕的变化而变化的font-size插件
-import '@/styles/index.less'
+import '@/styles/index.less' // 引入全局样式以及vant的覆盖样式
 
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 
