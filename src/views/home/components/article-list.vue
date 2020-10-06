31f21306cf84505f69666a5d8e6e73bb4bc8a398
周健煌 <1127893332@qq.com>
2020-10-06 23:00:44 +0800

    单图模式,三图模式,以及无图模式,他们的区别在于图片,下面的评论,数据条数都是一样的,把他们合并起来

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,29 +1,77 @@
 <template>
-<div class="scroll-wrapper">
-      <!-- 文章列表 -->
-      <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
+  <div class="scroll-wrapper">
+    <!-- 文章列表 -->
+    <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
        绑定的 loading 变成true
       -->
-    <van-list
-  v-model="upLoading"
-  :finished="finished"
-  finished-text="没有更多了"
-  @load="onLoad"
->
-<van-cell-group>
-  <van-cell v-for="item in articles" :key="item" title="美股熔断" :value="'内容' +item" />
+    <van-pull-refresh
+      v-model="isLoading"
+      @refresh="onRefresh"
+      loading-text="加载中"
+      :success-text="successText"
+    >
+      <van-list
+        v-model="upLoading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <van-cell-group>
+          <van-cell v-for="item in articles" :key="item">
+            <!-- 放置图片元素 ,有三图, 单图,无图 -->
 
-</van-cell-group>
-</van-list>
-</div>
+            <!-- 三图模式,因为下面的模块都一样,所以我们把三个模式合起来 -->
+            <div class="article_item">
+              <h3 class="van-ellipsis">
+                PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新
+              </h3>
+              <div class="img_box">
+                <van-image
+                  class="w33"
+                  fit="cover"
+                  src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+                <van-image
+                  class="w33"
+                  fit="cover"
+                  src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+                <van-image
+                  class="w33"
+                  fit="cover"
+                  src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+              </div>
+               <div class="img_box">
+                <van-image
+                  class="w100"
+                  fit="cover"
+                  src="https://img.yzcdn.cn/vant/cat.jpeg"
+                />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close"><van-icon name="cross"></van-icon></span>
+              </div>
+            </div>
+
+          </van-cell>
+        </van-cell-group>
+      </van-list>
+    </van-pull-refresh>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      successText: '', // 加载成功的文案
       upLoading: false, // 表示是否开启了上拉加载 默认值false
       finished: false, // 表示 是否已经完成所有数据的加载
+      isLoading: false, // 下载刷新状态,表示是否在下拉刷新
       articles: [] // 定义一个数组
     }
   },
@@ -41,7 +89,10 @@ export default {
         // 如果此时记录已经大于100
         this.finished = true // 关闭加载
       } else {
-        const arr = Array.from(Array(15), (value, index) => this.articles.length + index + 1)
+        const arr = Array.from(
+          Array(15),
+          (value, index) => this.articles.length + index + 1
+        )
         // 上拉加载 不是覆盖之前的数据  应该把数据追加到数组的队尾
         this.articles.push(...arr)
         // 添加完数据 需要手动的关掉 loading
@@ -50,11 +101,61 @@ export default {
       // setTimeout(() => {
       //   this.finished = true// 表示 数据已经全部加载完毕 没有数据了
       // }, 1000)
+    },
+    onRefresh () {
+      setTimeout(() => {
+        const arr = Array.from(
+          Array(5),
+          (value, index) => '天台排队' + (index + 1)
+        )
+        this.articles.unshift(...arr)
+        this.successText = `更新了${arr.length}条数据`
+        this.isLoading = false
+      }, 1000)
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
